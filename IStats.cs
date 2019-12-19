4786f9d77918d9a24c477be7fa61b0376d1d5cd4
Mental Refinery <mentalrefinery@gmail.com>
2019-12-20 01:09:09 +0200

    Revert "Attritubes classes Attack methods"
    This reverts commit 67b9461aaa0b7837296092a6bf44da8f54e85469.

--- IStats.cs
+++ IStats.cs
@@ -10,29 +10,13 @@ namespace GitHub_Masters__Praktika_
     {
         ///Visos galimos Herojaus ir padarų Charakteristikos.
 
-        //Attributai
-        int Constitution { get; set; }//Every X +1Health
-        //Jėga
-        int Strength { get; set; } // every X +1 Attack
-        //Vikrumas
-        int Dexterity { get; set; }//Every X +0.01% to dodge
-        //Sekme
-        int Luck { get; set; }//Crit rate and crit damage ++
-        //Meistriskumas
-        int Mastery { get; set; }//Increase damage or increase chance to hit ??
-
-
-        //Stats        
+        //Gyvybės
         int Health { get; set; }
-        int ChanceToDodge { get; set; }
-        int MinimumDamage { get; set; }
-        int MaximumDamage { get; set; }
+        //Jėga
+        int Strength { get; set; }
+        //Vikrumas
+        int Dexterity { get; set; }
 
-
-
-        //Methods
-        //Nuskaityt Atributu reiksmes ir atitinkamai paverst jas i stats
-        void AssignHeroStats();
 
 
     }
--- Mage.cs
+++ Mage.cs
@@ -6,23 +6,17 @@ using System.Threading.Tasks;
 
 namespace GitHub_Masters__Praktika_
 {
-    internal class Monster : IMonster
+    class Mage : IHero
     {
-        //Still in development
+        public int HeroLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double HeroExperience { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int Health { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
         public int Strength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int Dexterity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public void Attack(IHero hero)
+        public void GetDamage(int dmg)
         {
-            hero.GetDamage(RandomDamage());
-        }
-
-        private int RandomDamage()
-        {
-            Random rng = new Random();
-            return rng.Next();
+            throw new NotImplementedException();
         }
     }
 }
