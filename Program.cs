f4738150f97830d2b5c304720fa0c9d4928f33bf
Mental Refinery <mentalrefinery@gmail.com>
2019-12-20 01:14:03 +0200

    Changed Attributes Classes No more one shotting hero!

--- Program.cs
+++ Program.cs
@@ -17,13 +17,14 @@ namespace GitHub_Masters__Praktika_
             Console.WriteLine("Adventure begins");
             Console.ReadLine();
         }
+
         private static void HiAdventurer(string greeting)
         {
             Console.WriteLine(greeting);
             string adventurerName = Console.ReadLine();
             Console.WriteLine($"Hello {adventurerName}.");
-
         }
+
         private static void PlayOrNot(string question)
         {
             Console.WriteLine($"{question} Y / N?");
